78812bbef6e7b5f3e0c812d2e5dea3e5fcdbe69b
Sobilo34 <bilalsolih60@gmail.com>
2024-06-24 20:37:24 +0100

    Rewrite the following standard function to use ES6’s arrow syntax of the function add

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,13 @@
+#!/usr/bin/node
+
+// Arrow functions are a more concise way to write a function in JavaScript.
+
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = function add(newNeighborhood) {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  }
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
