f599952eb866c5046de651a07321b984796f4b55
Sobilo34 <bilalsolih60@gmail.com>
2024-06-24 20:52:45 +0100

    made all files executables

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments(employeesList) {
+        return Object.keys(employeesList).length;
+      },
+    };
+  }
