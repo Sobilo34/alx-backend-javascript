132627b0e40eb683210f5e349102af0f3cee19c8
Sobilo34 <bilalsolih60@gmail.com>
2024-06-27 22:17:26 +0100

    fix eslint error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 export default function appendToEachArrayValue(array, appendString) {
-    const myArray = [];
-  
-    for (const idx of array) {
-      const value = idx;
-      myArray.push(appendString + value);
-    }
-  
-    return myArray;
+  const myArray = [];
+
+  for (const idx of array) {
+    const value = idx;
+    myArray.push(appendString + value);
   }
+
+  return myArray;
+}
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 export default function iterateThroughObject(reportWithIterator) {
-    return reportWithIterator.join(' | ');
-  }
+  return reportWithIterator.join(' | ');
+}
