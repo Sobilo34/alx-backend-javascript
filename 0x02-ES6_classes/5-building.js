abe959332a58c64a7e9789ab601df364bff72d31
Sobilo34 <bilalsolih60@gmail.com>
2024-06-27 20:23:30 +0100

    resolve eslint error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -13,7 +13,7 @@ export default class Building {
     }
     this._sqft = newSqft;
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 }
