a20b23756219cf50f9404a17ae0a3d55a1108b1b
Sobilo34 <bilalsolih60@gmail.com>
2024-06-28 00:26:31 +0100

    Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,7 @@
-#!/usr/bin/node
-
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, 1000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
 }
