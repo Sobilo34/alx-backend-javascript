ccd8575ca5bc233e029ea7de2d520b3871a2f781
Sobilo34 <bilalsolih60@gmail.com>
2024-06-28 02:25:23 +0100

    change the output to string

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
-#!/usr/bin/node
-
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((info) => {
-      console.log(`${info[0].body} ${info[1].firstName} ${info[1].lastName}`);
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
+
