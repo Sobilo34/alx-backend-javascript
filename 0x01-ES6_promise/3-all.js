3a75f6d7e33b9042256c8ce7b02dbafb592f3c0e
Sobilo34 <bilalsolih60@gmail.com>
2024-06-28 02:20:18 +0100

    test code with a file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    .then((info) => {
+      console.log(`${info[0].body} ${info[1].firstName} ${info[1].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
