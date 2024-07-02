98116dd59689315812b955131119fde51b14c19b
Sobilo34 <bilalsolih60@gmail.com>
2024-07-02 18:37:19 +0100

    update the code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,30 +1,16 @@
-export default function updateGradeByCity(list, city, newGrade) {
-  // returns a new list of students with the updated grade
-  return list.map((item) => {
-    if (item.location === city) {
-        return{...item, grade: newGrade};
-    }
-    return item;
-    });
-}
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    // Check if students and newGrades are arrays and are not empty
-    if (
-      !Array.isArray(students)
-      || students.length === 0
-      || !Array.isArray(newGrades)
-      || newGrades.length === 0
-    ) {
-      return [];
-    }
-  
-    const studentsByCity = students.filter((std) => std.location === city);
-  
-    return studentsByCity.map((student) => {
-      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: studentGrade ? studentGrade.grade : 'N/A',
-      };
-    });
+  const defaultGrade = { grade: 'N/A' };
+
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .find((grade) => grade.studentId === student.id) || defaultGrade).grade,
+      }));
   }
+  return [];
+}
