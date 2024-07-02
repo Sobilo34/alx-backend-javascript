51ed9a4e56cc8f8494879132daffcc8d93df4542
Sobilo34 <bilalsolih60@gmail.com>
2024-07-02 18:35:15 +0100

    revert back

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,4 @@
-export default function getStudentsByLocation(students, city) {
-  if (students instanceof Array) {
-    return students.filter((student) => student.location === city);
-  }
-  return [];
+export default function getStudentsByLocation(list, city) {
+  // returns a filtered list of students who are in the same city
+  return list.filter((student) => student.location === city);
 }
