58d2d529bc62d8571c4a8489bbd513d0de6540b5
Sobilo34 <bilalsolih60@gmail.com>
2024-08-20 02:32:52 +0100

    Create a program named 1-stdin.js that will be executed through command line:
    It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
    The user should be able to input their name on a new line
    The program should display Your name is: INPUT
    When the user ends the program, it should display This important software is now closing (followed by a new line)

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// Create a program named 1-stdin.js that will be executed through command line:
+
+// It should display the message Welcome to Holberton School, what is your name?
+// The user should be able to input their name on a new line
+// The program should display Your name is: INPUT
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+    if (data != null) {
+        process.stdout.write(`Your name is: ${data}`);
+    }
+});
+// When the user ends the program, it should display This important software is now closing
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
