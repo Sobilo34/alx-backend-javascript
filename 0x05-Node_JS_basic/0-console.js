776ccd1a362da651a8a1e0a284728de3509ac787
Sobilo34 <bilalsolih60@gmail.com>
2024-08-20 04:08:40 +0100

    just a test

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /**
  * displayMessage - Prints the string argument to STDOUT
- * @arg: The string to print
+ * @text: The string to print
 */
 
-function displayMessage(arg) {
-    console.log(arg)
-}
-
-module.exports = displayMessage;
+function displayMessage(text) {
+    console.log(text);
+  }
+  
+  module.exports = displayMessage;
