8e2a7aeaeb270be29734e6c7d95f22a24cdbf2f4
Sobilo34 <bilalsolih60@gmail.com>
2024-08-22 08:17:16 +0800

    In a file named 5-http.js, create a small HTTP server using the http module:
    It should be assigned to the variable app and this one must be exported
    HTTP server should listen on port 1245
    It should return plain text
    When the URL path is /, it should display Hello Holberton School! in the page body
    When the URL path is /students, it should display This is the list of our students followed by the same content as the file 3-read_file_async.js (with and without the database) - the name of the database must be passed as argument of the file
    CSV file can contain empty lines (at the end) - and they are not a valid student!

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const { createServer } = require('http');
+
+const port = 1245;
+const hostname = '127.0.0.1';
+const database = process.argv.length > 2 ? process.argv[2] : '';
+const countStudents = require('./count_students')
+
+const app = createServer((req, res) => {
+    req.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    if (req.url === '/') {
+        res.end('Hello Holberton School!')
+    }
+    if (req.url === '/students') {
+        res.write('This is the list of our students\n');
+        countStudents(database.toString()).then((output) => {
+          const outString = output.slice(0, -1);
+          res.end(outString);
+        }).catch(() => {
+          res.statusCode = 404;
+          res.end('Cannot load the database');
+        });
+      }
+    });
+
+// Start the server and listen on the defined port
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
