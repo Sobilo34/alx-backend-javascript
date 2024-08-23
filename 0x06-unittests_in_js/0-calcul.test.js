d09db5481f77879c2fcae35234eb5434e3952d01
Sobilo34 <bilalsolih60@gmail.com>
2024-08-24 04:09:34 +0800

    updated the test file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,14 +5,17 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return 4 when inputs are 1 and 3', () => {
+    // test for int and int
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
   it('should return 5 when inputs are 1 and 3.7', () => {
+    // test for int and float
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
   it('should return 5 when inputs are 1.2 and 3.7', () => {
+    // test for float and float
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
