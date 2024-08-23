ae8474706e11723f96c90e619f0e0e0ae6d139ab
Sobilo34 <bilalsolih60@gmail.com>
2024-08-24 07:02:51 +0800

    use if statement instead of swich

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,22 +1,17 @@
 // 2-calcul.js
 
+
 function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-  
-    switch (type) {
-      case 'SUM':
+    if (type === 'SUM') {
         return Math.round(a) + Math.round(b);
-      case 'SUBTRACT':
+    } else if (type === 'SUBTRACT') {
         return Math.round(a) - Math.round(b);
-      case 'DIVIDE':
+    } else if (type === 'DIVIDE') {
         if (Math.round(b) === 0) {
-          return 'Error';
+            return 'Error';
         }
-        return roundedA / roundedB;
-      default:
-        break;
+        return Math.round(a) / Math.round(b);
     }
-  }
-  
-  module.exports = calculateNumber;
+}
+
+module.exports = calculateNumber;
