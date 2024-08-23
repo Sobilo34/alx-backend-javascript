36bb34be7231b4f82fd7f610b031a994aefc7cc7
Sobilo34 <bilalsolih60@gmail.com>
2024-08-24 06:28:49 +0800

    resolve version compatibility error and keep usinf CommanJS syntax

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,14 +1,17 @@
 // 2-calcul.js
 
-export default function calculateNumber(type, a, b) {
+
+function calculateNumber(type, a, b) {
     if (type === 'SUM') {
-      return Math.round(a) + Math.round(b);
+        return Math.round(a) + Math.round(b);
     } else if (type === 'SUBTRACT') {
-      return Math.round(a) - Math.round(b);
+        return Math.round(a) - Math.round(b);
     } else if (type === 'DIVIDE') {
-      if (Math.round(b) === 0) {
-        return 'Error';
-      }
-      return Math.round(a) / Math.round(b);
+        if (Math.round(b) === 0) {
+            return 'Error';
+        }
+        return Math.round(a) / Math.round(b);
     }
-  }
+}
+
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,14 +1,14 @@
 // 2-calcul.test.js
 
-import { expect } from 'chai';
-import calculateNumber from './2-calcul_chai.js';
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
   it('should return 6 when inputs are 1.4 and 4.5', () => {
     // test for int and int
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
-  
+
   it('should return -4 when inputs are 1.4 and 4.5', () => {
     // test for int and float
     expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
